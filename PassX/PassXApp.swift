c3c0d8ea4f1df0167162a3683c70fb5c53ab89d9
nathan <nathankdgray@gmail.com>
2023-07-03 18:36:29 -0700

    Initial Commit

---
+++ PassX/PassXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassXApp.swift
+//  PassX
+//
+//  Created by nate on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PassXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
