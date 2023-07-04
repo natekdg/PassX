de7990858f3a44240542389d4d4a0c610f4edfed
nathan <nathankdgray@gmail.com>
2023-07-03 20:17:58 -0700

    -toggle switch added for longer password functionality -ui cleanup

--- PassX/PassX Main.swift
+++ PassX/PassX Main.swift
@@ -8,36 +8,62 @@
 import SwiftUI
 
 struct PasscodeGeneratorView: View {
-    @State private var passcode: String = ""
+    @State private var passcode: String = ""    //  line declares a state property called 'passcode'
+    @State private var useLongPasscode: Bool = false    //  declares the state of the toggle switch for generating a long passcode (always set off)
+        @State private var clearPasscode: Bool = false
     
     var body: some View {
         VStack(spacing: 16) {
-            Text(passcode)
-                .font(.system(size: 24, weight: .bold))
-                .foregroundColor(.white)
+            Text("Your Passcode")
+                .font(.largeTitle)
+                .foregroundColor(.orange)
             
-            //generate passcode button
-            Button(action: generatePasscode) {
-                Text("Generate Secure Passcode")
-                    .font(.system(size: 18))
+            Text(passcode)
+                .font(.system(size: useLongPasscode ? 23 : 23))
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.black)
+                .cornerRadius(8)
+            
+            Toggle(isOn: $useLongPasscode) {
+                Text("Use 20 Characters")
+                    .font(.title)
                     .foregroundColor(.white)
-                    .padding()
+            }
+            .toggleStyle(SwitchToggleStyle(tint: .orange))
+            
+            Button(action: generatePasscode) {
+                Text("Generate")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
                     .background(Color.orange)
                     .cornerRadius(8)
             }
         }
         .padding()
         .background(Color.black)
-        .onAppear {
-            generatePasscode()
+        .edgesIgnoringSafeArea(.all)
+        .onChange(of: useLongPasscode) { newValue in
+            clearPasscode = newValue
+        }
+        .onChange(of: clearPasscode) { newValue in
+            if newValue {
+                passcode = ""
+                clearPasscode = false
+            }
         }
     }
-    
     //password generation fuction
     private func generatePasscode() {
+        let passcodeLength: Int = useLongPasscode ? 20 : 10
         let characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        let passcode = String((0..<10).map { _ in characters.randomElement()! })
+        let passcode = String((0..<passcodeLength).map { _ in characters.randomElement()! })
         self.passcode = passcode
+        
     }
 }
 
