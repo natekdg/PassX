d2b2b4c20bb2736f760e8ffaf52afbe1ae493e4a
nathan <nathankdgray@gmail.com>
2023-10-19 19:59:59 -0700

    Fixed Ui and Added Functions
    - fixed and aligned generate button
    - added copy function for generated passcode
    - use fonts for all text used in application

--- PassX/PassX Main.swift
+++ PassX/PassX Main.swift
@@ -1,11 +1,14 @@
 import SwiftUI
+import UIKit
+import UniformTypeIdentifiers
+
 
 struct PasscodeGeneratorView: View {
-    @State private var passcode: String = "" // declare for passcode
-    @State private var useLongPasscode: Bool = false // declare for 20 character option
-    @State private var clearPasscode: Bool = false // declare for clearing passcode when the switch is activated
-    @State private var isButtonAnimating: Bool = false // declare animation for password generation button
-    @State private var isPasscodeAnimating: Bool = false // declare animation for passcode that is generated
+    @State private var passcode: String = ""            // declare for passcode
+    @State private var useLongPasscode: Bool = false            // declare for 20 character option
+    @State private var clearPasscode: Bool = false          // declare for clearing passcode when the switch is activated
+    @State private var isButtonAnimating: Bool = false          // declare animation for password generation button
+    @State private var isPasscodeAnimating: Bool = false            // declare animation for passcode that is generated
     @State private var savedPasswords: [String] = []
     @State private var showSavedPasswords: Bool = false
     
@@ -17,29 +20,37 @@ struct PasscodeGeneratorView: View {
                 
                 VStack(spacing: 16) {
                     List {
-                        Section(header: Text("Options").font(.headline).fontWeight(.regular).foregroundColor(.white)) {
-                            OptionsView(useLongPasscode: $useLongPasscode, clearPasscode: $clearPasscode, passcode: $passcode) // options grid
+                        Section(header: 
+                            Text("Options")
+                            .font(.custom("Helvetica.ttc", size: 25))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)) {
+                            OptionsView(useLongPasscode: $useLongPasscode, clearPasscode: $clearPasscode, passcode: $passcode)          // options grid
                         }
                     }
-                    .listStyle(PlainListStyle()) // plain list style (minimal)
+                    .listStyle(PlainListStyle())            // plain list style (minimal)
                     
-                    VStack { // Wrap the text and passcode in a VStack
-                        Text("Your Passcode")
-                            .font(.custom("Helvetica.ttc", size: 30))
+                    VStack {            // wrap the text and passcode in VStack
+                        Text("Your Passcode")           // the text for "Your Passcode"
+                            .font(.custom("Helvetica.ttc", size: 30))           // custom font from assets
                             .fontWeight(.bold)
                             .foregroundColor(.white)
-                            .frame(height: 29.0) // fixed height
-                            .padding(.bottom, 16) // bottom padding
+                            .frame(height: 29.0)            // fixed height
+                            .padding(.bottom, 16)           // bottom padding
                         
                         Text(passcode) //generated passsword text
-                            .font(.custom("Helvetica.ttc", size: 20))
+                            .font(.custom("Helvetica.ttc", size: 23))
                             .fontWeight(.semibold)
                             .foregroundColor(.white)
                             .padding(.bottom, 6.0)
+                            .onTapGesture(count: 2) {
+                                let clipboard = UIPasteboard.general
+                                clipboard.setValue(passcode, forPasteboardType: UTType.plainText.identifier)        // function for copying the password
+                            }
                             .frame(width: 398.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/92.0)
                             .background(Color.black)
                             .cornerRadius(0)
-                            .animation(.easeInOut(duration: 0.35))
+                            .animation(.easeInOut(duration: 0.35))          // animation for the generated password to fade in
                     }
                     
                     
@@ -59,27 +70,31 @@ struct PasscodeGeneratorView: View {
                         
                         
                         Spacer()
-                            .padding(-12.0)
                             .frame(width: 61.0)
+                        
                         Button(action: {
                             generatePasscode() // actions for button
                             animateButton()
                             savePasscode()
                         }) {
-                            Text("Generate") // button ui
+                            Text("Generate") // button UI
                                 .font(.custom("Helvetica.ttc", size: 30))
                                 .fontWeight(.semibold)
                                 .foregroundColor(.white)
-                                .padding(.horizontal, 13.0)
+                                .padding(.horizontal, 25)
                                 .padding(.vertical, 15)
-                                .background(LinearGradient(gradient: Gradient(colors: [Color.teal, Color.purple]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
-                                .cornerRadius(20)
+                                .background(LinearGradient(gradient: Gradient(colors: [Color.teal, Color.purple]), startPoint: .leading, endPoint: .trailing))
+                                .opacity(20) // Adjusted opacity value
+                                .cornerRadius(15)
                         }
-                        
+                        .padding(.trailing, 12.0) // This shifts the button to the left
+
                         Spacer()
+
                         
                             .scaleEffect(isButtonAnimating ? 0.9 : 1.0) // button animation
                             .animation(.spring())
+                        
                     }
                     .padding()
                     .background(
@@ -96,7 +111,7 @@ struct PasscodeGeneratorView: View {
         
         private func generatePasscode() {
             let passcodeLength: Int = useLongPasscode ? 20 : 10
-            let characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" // Characters able to be used in password generation
+            let characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!?@" // Characters able to be used in password generation
             let passcode = String((0..<passcodeLength).map { _ in characters.randomElement()! })
             self.passcode = passcode
             
@@ -109,7 +124,7 @@ struct PasscodeGeneratorView: View {
         
         private func animateButton() {
             isButtonAnimating = true
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
                 isButtonAnimating = false
             }
         }
@@ -128,7 +143,7 @@ struct PasscodeGeneratorView: View {
         var body: some View {
             Toggle(isOn: $useLongPasscode) {
                 Text("20 Characters")
-                    .font(.title)
+                    .font(.custom("Helvetica.ttc", size: 23))
                     .foregroundColor(.white)
             }
             .toggleStyle(SwitchToggleStyle(tint: .purple))
@@ -153,14 +168,15 @@ struct SavedPasswordsView: View {
                 
                 ForEach(savedPasswords, id: \.self) { password in
                     Text(password)
-                        .foregroundColor(.orange)
+                        .foregroundColor(.white)
+                        .font(.custom("Helvetica.ttc", size: 16))
                         .listRowBackground(Color.black) // Sets the background of each list to be black
                 }
                 .onDelete(perform: deletePassword)
             }
             .listStyle(DefaultListStyle())
             .navigationBarTitle("Saved Passwords")
-            .foregroundColor(.orange)
+            .foregroundColor(.white)
             .navigationBarTitleDisplayMode(.inline)
         }
         .navigationViewStyle(StackNavigationViewStyle())
